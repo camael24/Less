60c2bbc5ea5cd8272c95dcca08ad076e5ef8f58c
camael24 <thehawk@hoa-project.net>
2012-11-13 12:34:56 +0100

    Add class simple syntax support within mixin structure

--- Grammar.pp
+++ Grammar.pp
@@ -1,5 +1,7 @@
 %skip   space                    \s
 %skip   comment:space            \s
+%skip   block:newline           \n
+%skip   block:retchar           \r
 %skip   hash                     #
 %skip   comma                    ,
 
@@ -7,10 +9,19 @@
 %token  colon                    :                  -> string
 %token  string:colon             :                  -> string
 %token  semicolon                ;                  -> default
+
 %token  string:semicolon         ;                  -> default
-%token  string:string            [^:;\r\n]+          -> default
-%token  comment:string           [^\r\n]+             -> default
+%token  string:string            [^:;\r\n]+         -> default
+%token  comment:string           [^\r\n]+           -> default
 %token  comment                  //                 -> comment
+
+%token  class                    [^{]+
+
+%token  brace_                   {                  -> block
+%token  block:string             [^:;}{]+           -> block
+%token  block:colon              :                  -> block
+%token  block:semicolon          ;                  -> block
+%token  block:_brace             }                  -> default
 
 
 
@@ -19,24 +30,34 @@
      (
         variable()
       | commentLine()
+      | block()
      )*
 
 #variable:
      (
-         ::arobase:: key() ::colon:: value() ::semicolon::
-     )+
+         ::arobase::  key() ::colon:: value() ::semicolon::
+     )
+
+#block:
+    <class> ::brace_:: blockKey()* ::_brace::
 
 
 #commentLine:
     ::comment:: comment()
 
-#comment:
-   <string>
+#blockKey:
+    <string> ::colon:: <string> ::semicolon::
 
-#key:
+#mixKey:
+    <string> ::colon:: <string> ::semicolon::
+
+comment:
     <string>
 
-#value:
+key:
+    <string>
+
+value:
     <string>
 
 
