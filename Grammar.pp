fa313abdd7f8a6ba34c2d8ad14d42a863c27cb88
camael24 <thehawk@hoa-project.net>
2012-11-14 08:47:41 +0100

    Variable, Comment, Block

--- Grammar.pp
+++ Grammar.pp
@@ -1,61 +1,44 @@
 %skip   space                    \s
+%skip   string:space             \s
 %skip   comment:space            \s
-%skip   block:newline           \n
-%skip   block:retchar           \r
+%skip   block:space              \s
 %skip   hash                     #
 %skip   comma                    ,
 
 // Variable
-
 %token  arobase                  @                  -> string
 %token  colon                    :                  -> string
 %token  string:colon             :
 %token  semicolon                ;
-
 %token  string:semicolon         ;                  -> default
 %token  string:string            [^:;\r\n]+         -> default
 
 // Comment
-
 %token  comment:string           [^\r\n]+           -> default
 %token  comment                  //                 -> comment
 
 // Block class
-
 %token  class                    [^{]+
 %token  brace_                   {                  -> block
-%token  block:string             [^:;}{]+
+%token  block:string             [^:;}]+
 %token  block:colon              :
 %token  block:semicolon          ;
 %token  block:_brace             }                  -> default
 
-
-
-
 #parsing:
-     (
-        variable()
-      | commentLine()
-      | block()
-     )*
+     ( variable() | commentLine() | block() )*
 
 #variable:
-     (
-         ::arobase::  key() ::colon:: value() ::semicolon::
-     )
+     ( ::arobase::  key() ::colon:: value() ::semicolon:: )
 
 #block:
     <class> ::brace_:: blockKey()* ::_brace::
-
 
 #commentLine:
     ::comment:: comment()
 
 #blockKey:
-    <string> ::colon:: <string> ::semicolon::
-
-#mixKey:
-    <string> ::colon:: <string> ::semicolon::
+    <string> ::colon:: <string>  ::semicolon::
 
 comment:
     <string>
@@ -65,5 +48,3 @@ key:
 
 value:
     <string>
-
-
