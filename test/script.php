8c426a2d3f516873012366a9880a6ddc820eb75e
camael24 <thehawk@hoa-project.net>
2013-05-06 10:56:47 +0200

    Modification on suite test

--- test/script.php
+++ test/script.php
@@ -8,39 +8,39 @@
         ->import('Console.Chrome.Style');
 
 
+    $store  = array();
+    $return = null;
     function test ($bool) {
-        if($bool === true)
-            return \Hoa\Console\Chrome\Style::stylize('[SUCCESS]', \Hoa\Console\Chrome\Style::COLOR_FOREGROUND_GREEN);
+        if(is_bool($bool))
+            if($bool === true)
+                return \Hoa\Console\Chrome\Style::stylize('[SUCCESS]', \Hoa\Console\Chrome\Style::COLOR_FOREGROUND_GREEN);
+            else
+                return \Hoa\Console\Chrome\Style::stylize('[FAIL]', \Hoa\Console\Chrome\Style::COLOR_FOREGROUND_RED);
         else
-            return \Hoa\Console\Chrome\Style::stylize('[FAIL]', \Hoa\Console\Chrome\Style::COLOR_FOREGROUND_RED);
+            return $bool;
     }
+
+    $add = function ($file, $bool) use (&$rapport) {
+        $rapport[] = array(
+            $file,
+            test($bool)
+        );
+    };
 
     $less = new \Less();
     $less->setCompiler(Hoa\Compiler\Llk::load(new Hoa\File\Read('hoa://Application/Less.pp')));
     $less->addInputFile('hoa://Application/test/sandbox.less');
     $less->addInputDirectory('hoa://Application/test/less/');
 
-    $rapport = array(
-        array(
-            'File',
-            'Result'
-        )
-    );
+    $add('File', 'Result');
 
-    $return = null;
     foreach ($less->getInputFiles() as $file) {
         $return = $less->validateFile($file);
         $bool   = (is_string($return)) ? false : $return;
 
-        $rapport[] = array(
-            $file,
-            test($bool)
-        );
-        if($bool === false) {
+        $add($file, $bool);
+        if($bool === false)
             break;
-        }
-
-
     }
 
     echo \Hoa\Console\Chrome\Text::columnize($rapport);
