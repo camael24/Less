30b470573becbb448694cad58c6379200e401491
camael24 <thehawk@hoa-project.net>
2013-05-15 16:55:08 +0200

    Go on composer, add the previous data after ;)

---
+++ src/hoathis/less/less.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by JetBrains PhpStorm.
+     * User: Julien
+     * Date: 15/05/13
+     * Time: 16:37
+     * To change this template use File | Settings | File Templates.
+     */
+    namespace Hoathis\Less {
+        class Less {
+            public function __construct () {
+                echo 'Bar Foo';
+            }
+        }
+    }
